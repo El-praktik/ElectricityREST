4243bf71af1e4344a72250b3e3be978eef525e56
Alex G. Jensen <jindoohex@hotmail.com>
2023-09-07 19:05:22 +0200

    Added test for MeasuresController

--- ElectricityRESTTests/Controllers/MeasuresControllerTests.cs
+++ ElectricityRESTTests/Controllers/MeasuresControllerTests.cs
@@ -1,20 +1,45 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.EntityFrameworkCore;
 using ElectricityREST.Controllers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ElectricityLibrary.model;
 
 namespace ElectricityREST.Controllers.Tests
 {
-    [TestClass()]
+    [TestClass]
     public class MeasuresControllerTests
     {
-        [TestMethod()]
+        private ELDBContext _context;
+        private MeasuresController _controller;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            // Use connectionstring to connect to the test database
+            var connectionString = @"data source=BLADE;initial catalog=ElPraktik;trusted_connection=true;Encrypt=False;";
+            var options = new DbContextOptionsBuilder<ELDBContext>()
+                .UseSqlServer(connectionString)
+                .Options;
+
+            _context = new ELDBContext(options);
+            _controller = new MeasuresController(_context);
+        }
+
+        [TestMethod]
         public void GetAllMeasuresTest()
         {
-            Assert.Fail();
+            // Act
+            var result = _controller.GetAllMeasures();
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            // Dispose of the database context after each test
+            _context.Dispose();
         }
     }
 }
