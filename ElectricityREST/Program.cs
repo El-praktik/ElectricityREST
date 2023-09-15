eb19e5d72f7a97d70fa461eeba8a383f20223a68
Zaritis <93724404+Zaritis@users.noreply.github.com>
2023-09-15 11:31:54 +0200

    New styles added for program

--- ElectricityREST/Program.cs
+++ ElectricityREST/Program.cs
@@ -6,15 +6,14 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddAuthorization();
 builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
 var key = Encoding.ASCII.GetBytes(new AppSettings().Secret);
@@ -47,7 +46,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseMiddleware<jwtmiddleware>();
 app.UseAuthorization();
 
 app.MapControllers();
